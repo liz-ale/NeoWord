0a3b07ea807515fbfdc86d941a11207bb89e2460
Lizbeth Alpizar <lizbeth.alejandro@neoris.com>
2024-09-30 14:57:51 -0600

    Add states in letterbox

--- NeoWord/NeoWord/WordGrid/LetterBoxView.swift
+++ NeoWord/NeoWord/WordGrid/LetterBoxView.swift
@@ -7,20 +7,84 @@
 
 import SwiftUI
 
+enum LetterState {
+    case idle
+    case zoom
+    case grayBackground
+    case greenBackground
+    case yellowBackground
+}
+
 struct LetterBoxView: View {
     var letter: String
+    @State private var currentState: LetterState = .idle
+    @State private var scale: CGFloat = 1.0
     
     var body: some View {
         Text(letter)
-            .frame(width: 50, height: 50)
-            //.background(Color.gray.opacity(0.2))
+            .frame(width: 60, height: 60)
+            .background(backgroundColor)
             .cornerRadius(5)
             .font(.title)
-            .foregroundColor(.black)
+            .foregroundColor(textColor)
             .overlay(
                 RoundedRectangle(cornerRadius: 5)
                     .stroke(Color.gray, lineWidth: 1)
             )
+            .scaleEffect(scale)
+            .onTapGesture {
+                triggerAnimation()
+            }
+            .animation(.easeInOut(duration: 0.3), value: scale)
+            .animation(.easeInOut(duration: 0.3), value: currentState)
+    }
+    
+    //Cambiar color según estado actual
+    private var backgroundColor: Color {
+        switch currentState {
+        case .idle:
+            return Color.white
+        case .zoom:
+            return Color.gray.opacity(0.2)
+        case .grayBackground:
+            return Color.gray
+        case .greenBackground:
+            return Color.green
+        case .yellowBackground:
+            return Color.yellow
+        }
+    }
+    
+    // Cambiar color del texto según estado actual
+    private var textColor: Color {
+        switch currentState {
+        case .idle, .zoom:
+            return Color.black
+        case .grayBackground, .greenBackground, .yellowBackground:
+            return Color.white
+        }
+    }
+    
+    // Activar la animación y cambiar el estado
+    private func triggerAnimation() {
+        switch currentState {
+        case .idle:
+            withAnimation {
+                scale = 0.8 // Zoom out
+                currentState = .zoom
+            }
+            withAnimation(Animation.easeInOut(duration: 0.3).delay(0.3)) {
+                scale = 1.1 // Zoom in
+            }
+        case .zoom:
+            currentState = .grayBackground
+        case .grayBackground:
+            currentState = .greenBackground
+        case .greenBackground:
+            currentState = .yellowBackground
+        case .yellowBackground:
+            currentState = .idle
+        }
     }
 }
 
--- NeoWord/NeoWord/WordGrid/LetterGridView.swift
+++ NeoWord/NeoWord/WordGrid/LetterGridView.swift
@@ -8,11 +8,11 @@
 import SwiftUI
 
 struct LetterGridView: View {
-    let columns: [GridItem] = Array(repeating: GridItem(.fixed(50)), count: 5)
+    let columns: [GridItem] = Array(repeating: GridItem(.fixed(60),spacing: 20), count: 5)
     let letters = Array(repeating: "", count: 30)
     
     var body: some View {
-        LazyVGrid(columns: columns, spacing: 10) {
+        LazyVGrid(columns: columns, spacing: 20) {
             ForEach(letters.indices, id: \.self) { index in
                 LetterBoxView(letter: letters[index])
             }
