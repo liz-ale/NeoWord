43de6e52d48cb60e413ba0b58fa10ef4acac612e
JulianGarciaArmengol <julian.garcia@neoris.com>
2024-10-04 11:27:16 -0600

    remove animation when tap

--- NeoWord/NeoWord/WordGrid/LetterGridView.swift
+++ NeoWord/NeoWord/WordGrid/LetterGridView.swift
@@ -13,11 +13,6 @@ struct LetterGridView: View {
     
     let columns: [GridItem] = Array(repeating: GridItem(.fixed(60), spacing: 10), count: 5)
     
-    // La palabra "today" en las primeras 5 posiciones, las demás vacías
-    let letters = ["T", "O", "D", "A", "Y",
-                   "T", "R", "U", "N", "K"]
-    + Array(repeating: "", count: 20)
-    
     var body: some View {
         LazyVGrid(columns: columns, spacing: 20) {
             ForEach(0..<30, id: \.self) { index in
@@ -26,13 +21,7 @@ struct LetterGridView: View {
                 } else {
                     LetterBoxView(letterBox: nil)
                 }
-                
-                
             }
-//            
-//            ForEach(letters.indices, id: \.self) { index in
-//                LetterBoxView(letter: letters[index])
-//            }
         }
         .padding()
     }
